a4990e50378ab2b30de59ee881006adaa31033f7
Przemysław Klejno <shizzed@pop-os.localdomain>
2021-07-20 11:42:07 +0200

    Naprawa hamburger menu, nadal brzydko się wyświetla menu

--- docs/src/app.js
+++ docs/src/app.js
@@ -3,6 +3,9 @@ const navList = document.getElementById('nav-list')
 
 function toggleButton(){
     navList.classList.toggle('show')
+    console.log('click');
+    hamburgerButton.classList.toggle('show');
+    hamburgerButton.classList.toggle('notActive');
 }
 
 hamburgerButton.addEventListener('click',toggleButton)
