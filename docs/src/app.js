54224d811b8886f670c04ef1abe43762062e259c
Przemysław Klejno <shizzed@pop-os.localdomain>
2021-07-21 15:24:11 +0200

    Dodanie animacji

--- docs/src/app.js
+++ docs/src/app.js
@@ -2,16 +2,28 @@
 
 
 const logo = document.getElementsByClassName('logo');
+const info = document.getElementsByClassName('info');
+const headshot = document.getElementsByClassName('headshot');
 
 
-setTimeout(() => {    logo[0].classList.toggle('rotateClockWise')}, 200);
+setTimeout(() => { logo[0].classList.toggle('rotateClockWise') }, 200);
 
-logo[0].addEventListener('click',Rotate);
-logo[0].addEventListener('hover',Rotate);
+logo[0].addEventListener('click', Rotate);
+document.addEventListener('scroll', Move);
 
-function Rotate()
-{
+function Rotate() {
     console.log('rotate');
     logo[0].classList.toggle('rotateClockWise');
 
 }
+function Move() {
+    if (document.documentElement.scrollTop < 50) {
+        info[0].classList.remove('active');
+        headshot[0].classList.remove('active');
+    }
+    else if (document.documentElement.scrollTop >= info[0].getBoundingClientRect().bottom) {
+        info[0].classList.add('active');
+        headshot[0].classList.add('active');
+    }
+}
+
